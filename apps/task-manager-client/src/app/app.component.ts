f3bde0a5c001c8a163c845a5c0c5d70e37ab442c
LTames <tamess@protonmail.com>
2024-01-27 23:07:15 -0300

    refactor(client): replace angular material with primeng

--- apps/task-manager-client/src/app/app.component.ts
+++ apps/task-manager-client/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'task-manager-client';
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+    }
 }
