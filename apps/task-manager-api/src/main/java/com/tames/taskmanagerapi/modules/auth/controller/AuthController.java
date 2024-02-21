8c12982c60e1cfc3237d8b58c9407f7ca60e120c
LTames <tamess@protonmail.com>
2024-02-21 00:42:10 -0300

    feat(api): Add OpenAPI docs for AuthController

--- apps/task-manager-api/src/main/java/com/tames/taskmanagerapi/modules/auth/controller/AuthController.java
+++ apps/task-manager-api/src/main/java/com/tames/taskmanagerapi/modules/auth/controller/AuthController.java
@@ -4,6 +4,12 @@ import com.tames.taskmanagerapi.modules.auth.dto.AuthResponseDto;
 import com.tames.taskmanagerapi.modules.auth.dto.LoginRequestDto;
 import com.tames.taskmanagerapi.modules.auth.service.AuthService;
 import com.tames.taskmanagerapi.modules.user.dto.UserRequestDto;
+import com.tames.taskmanagerapi.shared.dto.ApiValidationResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -27,13 +33,23 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+    @Operation(summary = "User login", description = "Login a user")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successful login", content = @Content(schema = @Schema(implementation = AuthResponseDto.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(schema = @Schema(implementation = ApiValidationResponseDto.class)))
+    })
     public ResponseEntity<AuthResponseDto> login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
 
         return new ResponseEntity<>(authService.login(loginRequestDto), HttpStatus.OK);
     }
 
     @PostMapping("/register")
+    @Operation(summary = "User register", description = "Register a new user")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User registered successfully", content = @Content(schema = @Schema(implementation = AuthResponseDto.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(schema = @Schema(implementation = ApiValidationResponseDto.class)))
+    })
     public ResponseEntity<AuthResponseDto> register(@RequestBody @Valid UserRequestDto userRequestDto) {
-        return new ResponseEntity<>(authService.register(userRequestDto), HttpStatus.OK);
+        return new ResponseEntity<>(authService.register(userRequestDto), HttpStatus.CREATED);
     }
 }
