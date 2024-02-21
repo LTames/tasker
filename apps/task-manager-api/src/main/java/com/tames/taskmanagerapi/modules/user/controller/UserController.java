327f542cd31422d229c6fdc96f24da3942f4f22f
LTames <tamess@protonmail.com>
2024-02-21 01:01:43 -0300

    feat(api): Add OpenAPI docs for UserController

--- apps/task-manager-api/src/main/java/com/tames/taskmanagerapi/modules/user/controller/UserController.java
+++ apps/task-manager-api/src/main/java/com/tames/taskmanagerapi/modules/user/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.tames.taskmanagerapi.modules.user.controller;
 
+import com.tames.taskmanagerapi.modules.auth.dto.AuthResponseDto;
 import com.tames.taskmanagerapi.modules.user.dto.UpdateUserRequestDto;
 import com.tames.taskmanagerapi.modules.user.dto.UserResponseDto;
 import com.tames.taskmanagerapi.modules.user.service.UserService;
+import com.tames.taskmanagerapi.shared.dto.ApiValidationResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -23,11 +30,20 @@ public class UserController {
     }
 
     @GetMapping("/me")
+    @Operation(summary = "Get authenticated user", description = "Retrieves information about the currently authenticated user")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved user information", content = @Content(schema = @Schema(implementation = AuthResponseDto.class))),
+    })
     public ResponseEntity<UserResponseDto> getAuthenticatedUser(Authentication authentication) {
         return new ResponseEntity<>(userService.getUserByUsername(authentication.getName()), HttpStatus.OK);
     }
 
     @PutMapping()
+    @Operation(summary = "Update user information", description = "Updates information for the authenticated user")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User information successfully updated", content = @Content(schema = @Schema(implementation = AuthResponseDto.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(schema = @Schema(implementation = ApiValidationResponseDto.class)))
+    })
     public ResponseEntity<UserResponseDto> updateUser(@Valid @RequestBody UpdateUserRequestDto updateUserRequestDto, Authentication authentication) {
         return new ResponseEntity<>(userService.updateUser(updateUserRequestDto, authentication.getName()), HttpStatus.OK);
     }
