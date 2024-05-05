b0dfd766c18f0f756c49097820430226a845c9ce
LTames <tamess@protonmail.com>
2024-05-05 16:04:08 -0300

    feat: add bash script for creating private and public keys on startup

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
-docker-compose down
+cd apps/tasker-api
 
+if ! [ -f private.pem ] && ! [ -f public.pem ]; then
+  echo "Halo"
+
+  openssl genrsa -out keypair.pem 2048
+  openssl rsa -in keypair.pem -pubout -out public.pem
+  openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out private.pem
+
+  rm keypair.pem
+fi
+
+docker-compose down
 docker-compose up --build --force-recreate --remove-orphans
