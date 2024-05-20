7b4516344e0e0fbdc590245e13483ca6234c307c
Lucas Oselame Tames <106827472+LTames@users.noreply.github.com>
2024-05-20 01:27:58 -0300

    refactor: update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 cd apps/tasker-api
 
 if ! [ -f private.pem ] && ! [ -f public.pem ]; then
-  echo "Halo"
-
   openssl genrsa -out keypair.pem 2048
   openssl rsa -in keypair.pem -pubout -out public.pem
   openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out private.pem
